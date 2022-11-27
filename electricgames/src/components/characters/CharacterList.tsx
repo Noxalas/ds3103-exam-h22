2d8d8ce12fe5fab63116da3a525fdf06f89461b5
Noxalas-Desktop <NatanielWlo@gmail.com>
2022-11-27 23:45:22 +0100

    added char search

--- electricgames/src/components/characters/CharacterList.tsx
+++ electricgames/src/components/characters/CharacterList.tsx
@@ -3,27 +3,35 @@ import CharacterItem from "./CharacterItem";
 import CharacterService from "../../services/CharacterService";
 import { Row } from "react-bootstrap";
 
-const CharacterList = () => {
-  const [characters, setCharacters] = useState<any[]>([]);
+const CharacterList = () =>
+{
+    const [characters, setCharacters] = useState<any[]>([]);
+    const [filter, setFilter] = useState('');
 
-  useEffect(() => {
-    setcharactersFromService();
-  }, []);
+    useEffect(() =>
+    {
+        setcharactersFromService();
+    }, []);
 
-  const setcharactersFromService = async () => {
-    const charactersFromService = await CharacterService.getAllCharacters();
-    setCharacters(charactersFromService);
-  };
+    const setcharactersFromService = async () =>
+    {
+        const charactersFromService = await CharacterService.getAllCharacters();
+        setCharacters(charactersFromService);
+    };
 
-  const getCharacterItems = () => {
-    return characters.map((character, counter) => <CharacterItem key={`character-${counter}`} name={character.name} />);
-  };
+    const filteredCharacters = characters.filter((key: any) => key.name.includes(filter))
 
-  return (
-    <section>
-      <h1 className="display-6">Characters</h1>
-      <Row>{getCharacterItems()}</Row>
-    </section>
-  );
+    const getCharacterItems = () =>
+    {
+        return filteredCharacters.map((character, counter) => <CharacterItem key={`character-${counter}`} name={character.name} />);
+    };
+
+    return (
+        <section>
+            <h1 className="display-6">Characters</h1>
+            <input type="text" onChange={e => setFilter(e.target.value)} />
+            <Row>{getCharacterItems()}</Row>
+        </section>
+    );
 };
 export default CharacterList;
