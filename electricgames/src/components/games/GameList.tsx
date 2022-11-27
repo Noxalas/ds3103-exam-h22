b5e5a5088413b8e57f9b16dbebfddb25ee4de625
Noxalas-Desktop <NatanielWlo@gmail.com>
2022-11-27 23:42:18 +0100

    added game search

--- electricgames/src/components/games/GameList.tsx
+++ electricgames/src/components/games/GameList.tsx
@@ -2,28 +2,40 @@ import { useState, useEffect } from "react";
 import GameItem from "./GameItem";
 import GameService from "../../services/GameService";
 import { Row } from "react-bootstrap";
+import React from "react";
 
-const GameList = () => {
-  const [games, setGames] = useState<any[]>([]);
 
-  useEffect(() => {
-    setgamesFromService();
-  }, []);
 
-  const setgamesFromService = async () => {
-    const gamesFromService = await GameService.getAllGames();
-    setGames(gamesFromService);
-  };
+const GameList = () =>
+{
+    const [games, setGames] = useState<any[]>([]);
+    const [filter, setFilter] = useState('');
 
-  const getGameItems = () => {
-    return games.map((game, counter) => <GameItem key={`game-${counter}`} title={game.title} platform={game.platform} releaseYear={game.releaseYear} image={game.image} />);
-  };
+    useEffect(() =>
+    {
+        setgamesFromService();
+    }, []);
 
-  return (
-    <section>
-      <h1 className="display-6">Games</h1>
-      <Row>{getGameItems()}</Row>
-    </section>
-  );
+    const setgamesFromService = async () =>
+    {
+        const gamesFromService = await GameService.getAllGames();
+        setGames(gamesFromService);
+    };
+
+    const filteredGames = games.filter((key: any) => key.title.includes(filter))
+
+    const getGameItems = () =>
+    {
+        return filteredGames.map((game, counter) => <GameItem key={`game-${counter}`} title={game.title} platform={game.platform} releaseYear={game.releaseYear} image={game.image} />);
+    };
+
+
+    return (
+        <section>
+            <h1 className="display-6">Games</h1>
+            Search: <input name="search" value={filter} onChange={e => setFilter(e.target.value)} />
+            <Row>{getGameItems()}</Row>
+        </section>
+    );
 };
 export default GameList;
