cfb04fdceefd512cb1a7fcf95fdd4a4426b27acf
biroman <eivind-buodd@hotmail.com>
2022-11-27 20:11:42 +0100

    style

--- electricgames/src/components/games/GameItem.tsx
+++ electricgames/src/components/games/GameItem.tsx
@@ -4,10 +4,12 @@ import { Col } from "react-bootstrap";
 const GameItem = ({ title, platform, releaseYear, image }: IGame) => {
   return (
     <Col xs={6}>
-      <h3>{title}</h3>
-      <h4>{platform}</h4>
-      <h4>{releaseYear}</h4>
-      <img src={`http://localhost:5126/images/${image}`} alt="bilde" className="test" />
+      <div className="gameBorder">
+        <h3>{title}</h3>
+        <h4>{platform}</h4>
+        <h4>{releaseYear}</h4>
+        <img src={`http://localhost:5126/images/${image}`} alt="bilde" className="test" />
+      </div>
     </Col>
   );
 };
