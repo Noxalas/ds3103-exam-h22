44878314521b6b85d9f834cbba0e90922ea35e7e
biroman <eivind-buodd@hotmail.com>
2022-11-28 04:35:44 +0100

    -

--- electricgames/src/components/games/AddGame.tsx
+++ electricgames/src/components/games/AddGame.tsx
@@ -3,49 +3,46 @@ import axios from "axios";
 import ImageUpload from "./UploadImage";
 const url = "http://localhost:5126/api/game/post";
 
-function AddGame()
-{
-    const [title, setTitle] = useState("");
-    const [platform, setPlatform] = useState("");
-    const [releaseYear, setReleaseYear] = useState("");
-    const [image, setImage] = useState([]);
+function AddGame() {
+  const [title, setTitle] = useState("");
+  const [platform, setPlatform] = useState("");
+  const [releaseYear, setReleaseYear] = useState("");
+  const [image, setImage] = useState([]);
 
-    const handleImage = (img: any) =>
-    {
-        const el = document.getElementById('image') as HTMLInputElement;
-        el.placeholder = `${img.name}`;
-        setImage(img.name);
+  const handleImage = (img: any) => {
+    const el = document.getElementById("image") as HTMLInputElement;
+    el.placeholder = `${img.name}`;
+    setImage(img.name);
+  };
+
+  const handler = async (e: any) => {
+    e.preventDefault();
+    try {
+      const resp = await axios.post(url, { title: title, platform: platform, releaseYear: releaseYear, image: image });
+      console.log(resp.data);
+    } catch (error: any) {
+      console.log(error.response);
     }
+  };
 
-    const handler = async (e: any) =>
-    {
-        e.preventDefault();
-        try {
-            const resp = await axios.post(url, { title: title, platform: platform, releaseYear: releaseYear, image: image });
-            console.log(resp.data);
-        } catch (error: any) {
-            console.log(error.response);
-        }
-    };
-
-    return (
-        <section>
-            <form className="form" onSubmit={handler}>
-                <div className="mb-3"></div>
-                <input type="text" className="form-input " id="title" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} /> <br />
-                <input type="text" className="form-input" id="platform" placeholder="Platform" value={platform} onChange={(e) => setPlatform(e.target.value)} />
-                <br />
-                <input type="releaseYear" className="form-input" id="releaseYear" placeholder="Release year" value={releaseYear} onChange={(e) => setReleaseYear(e.target.value)} />
-                <br />
-                <input disabled type="text" className="form-input" id="image" placeholder="Image" />
-                <br />
-                <p>{image}</p>
-                <ImageUpload name="image" handleImage={handleImage} />
-                <button type="submit" className="btn btn-primary">
-                    register
-                </button>
-            </form>
-        </section>
-    );
-};
+  return (
+    <section>
+      <form className="form" onSubmit={handler}>
+        <div className="mb-3"></div>
+        <input type="text" className="form-input " id="title" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} /> <br />
+        <input type="text" className="form-input" id="platform" placeholder="Platform" value={platform} onChange={(e) => setPlatform(e.target.value)} />
+        <br />
+        <input type="releaseYear" className="form-input" id="releaseYear" placeholder="Release year" value={releaseYear} onChange={(e) => setReleaseYear(e.target.value)} />
+        <br />
+        <input disabled type="text" className="form-input" id="image" placeholder="Image" />
+        <br />
+        <p>{image}</p>
+        <ImageUpload name="image" handleImage={handleImage} />
+        <button type="submit" className="btn btn-primary">
+          register
+        </button>
+      </form>
+    </section>
+  );
+}
 export default AddGame;
--- electricgames/src/components/games/UploadImage.tsx
+++ electricgames/src/components/games/UploadImage.tsx
@@ -1,37 +1,32 @@
 import { useState, ChangeEvent } from "react";
 import ImageUploadService from "../../services/ImageUploadService";
 
+function ImageUpload({ handleImage }: any) {
+  const [image, setImage] = useState<File | null>(null);
 
-function ImageUpload({ handleImage }: any)
-{
-    const [image, setImage] = useState<File | null>(null);
+  const setImageHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if (files != null) {
+      const file = files[0];
+      handleImage(file);
+      setImage(file);
+    }
+  };
 
-    const setImageHandler = (e: ChangeEvent<HTMLInputElement>) =>
-    {
-        const { files } = e.target;
-        if (files != null) {
-            const file = files[0];
-            handleImage(file);
-            setImage(file);
-            console.log(file.name);
-        }
-    };
-
-    const uploadImage = () =>
-    {
-        if (image != null) {
-            ImageUploadService.uploadImage(image);
-        }
-    };
-    return (
-        <div>
-            <main>
-                <label>Velg bilde</label>
-                <input onChange={setImageHandler} type="file" />
-                <button onClick={uploadImage}>Last opp</button>
-            </main>
-        </div>
-    );
+  const uploadImage = () => {
+    if (image != null) {
+      ImageUploadService.uploadImage(image);
+    }
+  };
+  return (
+    <div>
+      <main>
+        <label>Velg bilde</label>
+        <input onChange={setImageHandler} type="file" />
+        <button onClick={uploadImage}>Last opp</button>
+      </main>
+    </div>
+  );
 }
 
 export default ImageUpload;
